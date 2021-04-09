d7ed06870776fe4a3877ef2c04f96ea38fb11793
mikenath223 <michgoldennathan17@gmail.com>
2021-04-09 10:19:34 +0100

    Add redux-mock store

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store'
 import App from './App';
 
-test('renders App', () => {
-  render(<App />);
-  const navElement = screen.getByText(/Users-Posts/i);
-  expect(navElement).toBeInTheDocument();
-});
+describe('App', function () {
+  const mockStore = configureStore()
+  const data = {
+    items: [], error: false, loading: true
+  }
+  const store = mockStore({
+    usersData: data,
+    postsData: data
+  })
+
+  test('renders App', () => {
+    render(<Provider store={store}>
+      <App />
+    </Provider>);
+    const navElement = screen.getByText(/Users-Posts/i);
+    expect(navElement).toBeInTheDocument();
+  });
+})
